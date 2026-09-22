8458b4ba8897b9dabdd29d3e44befdb347b6deab
FaDeAWAYut <fadeaway33799@gmail.com>
2026-09-22 15:44:23 +0200

    Initialize Welcome Page

--- LostApp/src/index.js
+++ LostApp/src/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const axios = require("axios");
+const path = require("path");
 
 const app = express();
+
+// Serve static files (including our HTML) from the public folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/items", async (req, res) => {
   try {
     const response = await axios.get("http://localhost:4000/api/items");
-    res.json(response.data); // or render a view with this data
+    res.json(response.data);
   } catch (err) {
     res.status(500).json({ error: "Could not reach LostService" });
   }
