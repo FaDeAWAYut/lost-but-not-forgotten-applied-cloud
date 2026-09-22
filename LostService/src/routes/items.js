00f5912a1b256d6ef103061c25c1ade6b9b32bee
FaDeAWAYut <fadeaway33799@gmail.com>
2026-09-22 17:01:14 +0200

    Added POST logic to found.html, LostApp index.js, LostService route, and Updated Item.js model

--- LostService/src/routes/items.js
+++ LostService/src/routes/items.js
@@ -2,17 +2,26 @@ const express = require("express");
 const router = express.Router();
 const Item = require("../model/Item");
 
-// get all items
 router.get("/", async (req, res) => {
   const items = await Item.find();
   res.json(items);
 });
 
-// get item by id
 router.get("/:id", async (req, res) => {
   const item = await Item.findById(req.params.id);
   if (!item) return res.status(404).json({ error: "Not found" });
   res.json(item);
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newItem = new Item(req.body);
+    const savedItem = await newItem.save();
+    console.log("Saved item:", savedItem);
+    res.status(201).json(savedItem);
+  } catch (err) {
+    res.status(400).json({ error: "Could not save item" });
+  }
 });
 
 module.exports = router;
